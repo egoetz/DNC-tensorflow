70cd21342fb3ce93184fe12268771300679fa510
egoetz <egoetz20@wooster.edu>
2019-12-16 19:31:38 -0500

    Fix test for foo task

--- tasks/foo/test.py
+++ tasks/foo/test.py
@@ -76,21 +76,16 @@ with graph.as_default():
         softmaxed = tf.nn.softmax(outputs)
 
         tasks_results = {}
-        tasks_names = {}
+        tasks_numbers = []
+        counter = 0
         for test_file in test_files:
             test_data = load(test_file)
-            task_regexp = r'([0-9]{1,2})test.txt.pkl'
-            print task_regexp
+            task_regexp = r'([0-9]{1,3})test.txt.pkl'
             task_filename = os.path.basename(test_file)
-            print task_filename
             task_match_obj = re.match(task_regexp, task_filename)
             task_number = task_match_obj.group(1)
-            task_name = task_match_obj.group(2).replace('-', ' ')
-            tasks_names[task_number] = task_name
-            counter = 0
+            tasks_numbers.append(task_number)
             results = []
-
-            llprint("%s ... %d/%d" % (task_name, counter, len(test_data)))
 
             for story in test_data:
                 a_story = np.array(story['inputs'])
@@ -118,32 +113,21 @@ with graph.as_default():
                         targets_cursor += 1
                     results.append(np.prod(question_grade))
 
-                counter += 1
-                llprint("\r%s ... %d/%d" % (task_name, counter, len(test_data)))
+            counter += 1
+            llprint("\rTests Completed ... %d/%d" % (counter, len(test_files)))
 
             error_rate = 1. - np.mean(results)
             tasks_results[task_number] = error_rate
-            llprint("\r%s ... %.3f%% Error Rate.\n" % (task_name, error_rate * 100))
-
         print "\n"
-        print "%-27s%-27s%s" % ("Task", "Result", "Paper's Mean")
+        print "%-27s%-27s" % ("Task", "Result")
         print "-------------------------------------------------------------------"
-        paper_means = {
-            '1': '9.0±12.6%', '2': '39.2±20.5%', '3': '39.6±16.4%',
-            '4': '0.4±0.7%', '5': '1.5±1.0%', '6': '6.9±7.5%', '7': '9.8±7.0%',
-            '8': '5.5±5.9%', '9': '7.7±8.3%', '10': '9.6±11.4%', '11': '3.3±5.7%',
-            '12': '5.0±6.3%', '13': '3.1±3.6%', '14': '11.0±7.5%', '15': '27.2±20.1%',
-            '16': '53.6±1.9%', '17': '32.4±8.0%', '18': '4.2±1.8%', '19': '64.6±37.4%',
-            '20': '0.0±0.1%', 'mean': '16.7±7.6%', 'fail': '11.2±5.4'
-        }
-        for k in range(20):
-            task_id = str(k + 1)
-            task_result = "%.2f%%" % (tasks_results[task_id] * 100)
-            print "%-27s%-27s%s" % (tasks_names[task_id], task_result, paper_means[task_id])
+        for task_number in sorted(tasks_numbers):
+            task_result = "%.2f%%" % (tasks_results[task_number] * 100)
+            print "%-27s%-27s" % (task_number, task_result)
         print "-------------------------------------------------------------------"
         all_tasks_results = [v for _, v in tasks_results.iteritems()]
         results_mean = "%.2f%%" % (np.mean(all_tasks_results) * 100)
         failed_count = "%d" % (np.sum(np.array(all_tasks_results) > 0.05))
 
-        print "%-27s%-27s%s" % ("Mean Err.", results_mean, paper_means['mean'])
-        print "%-27s%-27s%s" % ("Failed (err. > 5%)", failed_count, paper_means['fail'])
+        print "%-27s%-27s" % ("Mean Err.", results_mean)
+        print "%-27s%-27s" % ("Failed (err. > 5%)", failed_count)
