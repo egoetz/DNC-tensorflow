Fix cleaning bugs on words with ', units, serial numbers, and other symbols

tasks/foo/test.py
@@ -72,7 +72,7 @@ with graph.as_default():
             memory_read_heads=4,
         )
 
-        ncomputer.restore(session, ckpts_dir, 'step-665')
+        ncomputer.restore(session, ckpts_dir, 'step-100001')
 
         outputs, _ = ncomputer.get_outputs()
         softmaxed = tf.nn.softmax(outputs)